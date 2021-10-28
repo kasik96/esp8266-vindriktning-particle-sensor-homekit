d3c15134230ba3627547b8cf0163a03f7fa4015b
Martin K <martink@Martins-MacBook-Air.local>
2021-10-28 20:15:36 +0200

    Update

--- esp8266-vindriktning-particle-sensor-homekit.ino
+++ esp8266-vindriktning-particle-sensor-homekit.ino
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include <arduino_homekit_server.h>
-#include "wifi_info.h"
+#include <ArduinoOTA.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
 #include "SerialCom.h"
 #include "Types.h"
 
@@ -8,9 +12,13 @@
 
 particleSensorState_t state;
 
+WiFiManager wifiManager;
+WiFiClient wifiClient;
+
+char identifier[24];
+
 void setup() {
   Serial.begin(115200);
-  wifi_connect(); // in wifi_info.h
   my_homekit_setup();
 }
 
@@ -35,10 +43,44 @@ void my_homekit_setup() {
   arduino_homekit_setup(&config);
   Serial.begin(115200);
   SerialCom::setup();
+  delay(3000);
+  snprintf(identifier, sizeof(identifier), "VINDRIKTNING-%X", ESP.getChipId());
+  WiFi.hostname(identifier);
+  setupWifi();
+  setupOTA();
+}
+
+void setupOTA() {
+    ArduinoOTA.onStart([]() { Serial.println("Start"); });
+    ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        }
+    });
+
+    ArduinoOTA.setHostname(identifier);
+
+    // This is less of a security measure and more a accidential flash prevention
+    ArduinoOTA.setPassword(identifier);
+    ArduinoOTA.begin();
 }
 
 void my_homekit_loop() {
   arduino_homekit_loop();
+  ArduinoOTA.handle();
   SerialCom::handleUart(state);
   const uint32_t t = millis();
   if (t > next_report_millis) {
@@ -55,6 +97,18 @@ void my_homekit_loop() {
         ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
 
   }
+}
+
+void setupWifi() {
+    wifiManager.setDebugOutput(false);
+    WiFi.hostname(identifier);
+    wifiManager.autoConnect(identifier);
+}
+
+void resetWifiSettingsAndReboot() {
+    wifiManager.resetSettings();
+    delay(3000);
+    ESP.restart();
 }
 
 void my_homekit_report() {
@@ -74,8 +128,4 @@ void my_homekit_report() {
   cha_air_quality.value.int_value = air_quality_val;
   homekit_characteristic_notify(&cha_pm25_density, cha_pm25_density.value);
   homekit_characteristic_notify(&cha_air_quality, cha_air_quality.value);
-}
-
-int random_value(int min, int max) {
-  return min + random(max - min);
 }
